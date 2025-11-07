dc78385fa464d55d129163be7c4dae37d0fa3c44
Md. Arif Hassan <aarifhsn@gmail.com>
2025-11-07 22:25:34 +0600

    Refactored Testimonial sections with Icons

--- Assignment/rnext-first/src/components/Layout/Header/NavLink.jsx
+++ Assignment/rnext-first/src/components/Layout/Header/NavLink.jsx
@@ -2,7 +2,7 @@ export default function NavLink({ label }) {
   return (
     <>
       <div className="flex justify-start items-center">
-        <div className="flex flex-col justify-center text-[rgba(49,45,43,0.80)] text-xs md:text-[13px] font-medium leading-[14px] cursor-pointer">
+        <div className="flex flex-col justify-center text-[rgba(49,45,43,0.80)] text-xs md:text-[13px] font-medium leading-3.5 cursor-pointer">
           {label}
         </div>
       </div>
